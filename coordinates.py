56a3e7a7b334074bc12934b176357123e4d872ce
Sanjay P Lal <63526658+plalsanjay@users.noreply.github.com>
2023-07-07 08:57:39 +0530

    Update coordinates.py

--- coordinates.py
+++ coordinates.py
@@ -1,22 +1,16 @@
 import cv2
-
-
 def click_event(event, x, y, flags, params):
    if event == cv2.EVENT_LBUTTONDOWN:
       print(f'({x},{y})')
-      
       # put coordinates as text on the image
       cv2.putText(img, f'({x},{y})',(x,y),
       cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-      
       # draw point on the image
       cv2.circle(img, (x,y), 3, (0,255,255), -1)
- 
 # read the input image
 #img = cv2.imread('back2school.jpg')
 
 video = cv2.VideoCapture("/home/kaushal/Desktop/Videosss/20220122_184557.mp4")
-
 check,frame =video.read()
 
 # create a window
